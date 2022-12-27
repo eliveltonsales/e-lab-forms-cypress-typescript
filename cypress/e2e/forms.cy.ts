574661c4065f1acb3a98a2480e51f3b736c218a4
Elivelton Sales <eliveltoneos@gmail.com>
2022-12-27 11:41:28 -0300

    fix form ids

--- cypress/e2e/forms.cy.ts
+++ cypress/e2e/forms.cy.ts
@@ -10,25 +10,25 @@ describe('e-lab, forms',()=>{
     
     it('should disable save button without terms accepted',()=>{           
         cy.get('#btnSave').should('be.disabled')
-        cy.get('#chkPeopleTerms').click()
+        cy.get('#chkTerms').click()
         cy.get('#btnSave').should('be.enabled')
     })
 
     it('should show data warning after accepting terms and conditions',()=>{
-        cy.get('#chkPeopleTerms').click()
+        cy.get('#chkTerms').click()
         cy.get('#dataWarning').should('have.text',"The form data isn't stored anywhere!")
     })
 
     it('should be able to save form',()=>{
-        cy.get('#txtPeopleName').type('Teste')
-        cy.get('#txtPeopleNickname').type('Teste')
-        cy.get('#txtPeopleEmail').type('Teste@teste.com')
-        cy.get('#txtPeoplePassword').type('teste')
-        cy.get('#txtPeopleCitizenID').type('111')
-        cy.get('#txtPeoplePhone').type('111')
-        cy.get('#chkPeopleTerms').click()
+        cy.get('#txtName').type('Teste')
+        cy.get('#txtNickname').type('Teste')
+        cy.get('#txtEmail').type('Teste@teste.com')
+        cy.get('#txtPassword').type('teste')
+        cy.get('#txtCitizenID').type('111')
+        cy.get('#txtPhone').type('111')
+        cy.get('#chkTerms').click()
         cy.get('#btnSave').click()
-        cy.get('#txtPeopleName').should('be.empty')
+        cy.get('#txtName').should('be.empty')
         cy.get('#dataWarning').should('not.be.visible')
     })
 })
